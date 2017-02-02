44dbbccea2a9927af3568845444af7be3cbc909d
waxkinetic <wax@kinetic.com>
2017-02-02 09:57:21 -0800

    use logging, not print

--- awsdelmapimages/__main__.py
+++ awsdelmapimages/__main__.py
@@ -5,40 +5,50 @@ import json
 import logging
 from os import environ
 from subprocess import check_call
-import traceback
 
 # project.
 from awsdelmapimages.delete_map_images import delete_map_images
 
 
+_LOG_FILE_NAME = '/var/log/glassnetic.log'
+_logger = logging.getLogger()
+
+
 def copy_log(bucket, version, suffix):
     suffix = "" if suffix is None else '_' + suffix
-    print('copying: /var/log/cloud-init-output.log')
-    args = ['aws', 's3', 'cp', '/var/log/cloud-init-output.log',
+    _logger.info('copying GLASSNETIC log file.')
+    args = ['aws', 's3', 'cp', _LOG_FILE_NAME,
             's3://{}/maps/tasks/delete_map_images_{}{}.log'.format(bucket, version, suffix)]
+    check_call(args)
+
+    _logger.info('copying CLOUD-INIT log file.')
+    args = ['aws', 's3', 'cp', '/var/log/cloud-init-output.log',
+            's3://{}/maps/tasks/delete_map_images_{}_cloud_output.log'.format(bucket, version)]
     check_call(args)
 
 
 def main():
+    logging.basicConfig(level=logging.DEBUG, filename=_LOG_FILE_NAME)
+
     suffix = None
     config = dict(bucket='glsmap', version='ERROR')
     try:
         cfg_str = environ.get('SCRIPT_CONFIG', "{}")
-        print('config string: {}'.format(cfg_str))
+        _logger.info('config string: {}'.format(cfg_str))
 
         errors = 0
         cfg = json.loads(cfg_str)
         for k in ('region', 'bucket', 'version', 'max_zoom'):
             if k not in cfg:
-                print('** CONFIG ERROR: missing "{}"'.format(k))
+                _logger.info('** CONFIG ERROR: missing "{}"'.format(k))
                 errors += 1
 
         if errors == 0:
-            print('config: {}'.format(cfg))
+            _logger.info('config: {}'.format(cfg))
             config = cfg
             delete_map_images(config)
     except Exception:
-        traceback.print_exc()
+        _logger.exception('EXCEPTION')
         suffix = 'ERROR'
 
     # flush logs, then copy the log to S3 for review.
--- awsdelmapimages/delete_map_images.py
+++ awsdelmapimages/delete_map_images.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import
 
 
+import logging
 from multiprocessing.dummy import Pool
 from subprocess import check_output
 import boto3
 
+
+# module logger.
+_logger = logging.getLogger()
 
 # private data; also inherited by child processes.
 _cli = None
@@ -26,17 +30,17 @@ def delete_map_images(config):
     _max_zoom = config['max_zoom']
 
     # delete map tiles images; this generally takes awhile.
-    print('==================== START TILE DELETION:')
+    _logger.info('==================== START TILE DELETION:')
     pool = Pool(8)
     pool.map(_delete_by_index, range(2 ** _max_zoom), chunksize=8)
-    print('==================== END TILE DELETION.')
+    _logger.info('==================== END TILE DELETION.')
 
     # delete product images; this is pretty quick.
-    print('==================== DELETING PRODUCT IMAGE FILES:')
+    _logger.info('==================== DELETING PRODUCT IMAGE FILES:')
     args = ['aws', 's3', 'rm', '--recursive', '--only-show-errors',
             's3://{bucket}/maps/{version}/'.format(bucket=_bucket, version=_version)]
-    print(check_output(args))
-    print('==================== FINISHED: DELETED IMAGES FOR MAP VERSION "{}".'.format(_version))
+    _logger.info(check_output(args))
+    _logger.info('==================== FINISHED: DELETED IMAGES FOR MAP VERSION "{}".'.format(_version))
 
 
 def _generate_keys_for(y_hash, version, max_zoom):
@@ -52,7 +56,7 @@ def _generate_keys_for(y_hash, version, max_zoom):
     return hash, key_list
 
 
-def _delete_keys(s3_cli, bucket, keys):
+def _delete_keys(s3_cli, hash, bucket, keys):
     index = 0
     err_keys = []
     while index < len(keys):
@@ -60,18 +64,19 @@ def _delete_keys(s3_cli, bucket, keys):
         objects = map(lambda k: dict(Key=k), keys[index:index+count])
         index += count
         result = s3_cli.delete_objects(Bucket=bucket, Delete=dict(Objects=objects))
-        print('    {} (of {})'.format(len(result.get('Deleted', [])), len(objects)))
+        _logger.info('hash=0x{}: {} (of {})'.format(hash, len(result.get('Deleted', [])), len(objects)))
         for error in result.get('Errors', []):
             err_keys.append(error['Key'])
-            print('{code}: {key}'.format(code=error['Code'], key=error['Key']))
+            _logger.info('{code}: {key}'.format(code=error['Code'], key=error['Key']))
 
     if err_keys:
         # recursive call to retry failed keys.
-        print('    ** {} ERRORS; retrying'.format(len(err_keys)))
+        _logger.info('** {} ERRORS; retrying'.format(len(err_keys)))
         _delete_keys(s3_cli, bucket, err_keys)
 
 
 def _delete_by_index(index):
     hash, keys = _generate_keys_for(index, _version, _max_zoom)
-    print('========== 0x{} ({}):'.format(hash, index))
-    _delete_keys(_cli, _bucket, keys)
+    _logger.info('========== hash=0x{} ({}) [START] =========='.format(hash, index))
+    _delete_keys(_cli, hash, _bucket, keys)
+    _logger.info('========== hash=0x{} ({}) [END] =========='.format(hash, index))
